b64043a744b05c7413a2b48af266b7e1986ddd69
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-02 22:29:32 +0530

    seperated dashboard sidebar with component

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import Layout from '../common/Layout'
 import { AdminAuthContext } from '../context/AdminAuth'
+import Sidebar from '../common/Sidebar'
 
 const Dashboard = () => {
 
@@ -14,39 +15,7 @@ const Dashboard = () => {
             <h4 className='h4 pb-0 mb-0'>Dashboard</h4>
           </div>
           <div className="col-md-3">
-            <div className="card shadow sidebar">
-              <div className="card-body p-4">
-                <ul>
-                  <li>
-                    <a href="">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="">Categories</a>
-                  </li>
-                  <li>
-                    <a href="">Brands</a>
-                  </li>
-                  <li>
-                    <a href="">Products</a>
-                  </li>
-                  <li>
-                    <a href="">Orders</a>
-                  </li>
-                  <li>
-                    <a href="">Users</a>
-                  </li>
-                  <li>
-                    <a href="">Shipping</a>
-                  </li>
-                  <li>
-                    <a href="">Change Password</a>
-                  </li>
-                  <li>
-                    <a href="" onClick={logout} >Logout</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <Sidebar/>
           </div>
           <div className="col-md-9"></div>
         </div>
