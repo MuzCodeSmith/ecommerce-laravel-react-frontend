afdb0f1426b7370399abb19939bb515c12d68983
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-02 22:27:01 +0530

    dashboard sidebar part is completed

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -8,8 +8,49 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      <div>Dashboard</div>
-      <button onClick={logout} >Logout</button>
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-between mt-5 pb-3">
+            <h4 className='h4 pb-0 mb-0'>Dashboard</h4>
+          </div>
+          <div className="col-md-3">
+            <div className="card shadow sidebar">
+              <div className="card-body p-4">
+                <ul>
+                  <li>
+                    <a href="">Dashboard</a>
+                  </li>
+                  <li>
+                    <a href="">Categories</a>
+                  </li>
+                  <li>
+                    <a href="">Brands</a>
+                  </li>
+                  <li>
+                    <a href="">Products</a>
+                  </li>
+                  <li>
+                    <a href="">Orders</a>
+                  </li>
+                  <li>
+                    <a href="">Users</a>
+                  </li>
+                  <li>
+                    <a href="">Shipping</a>
+                  </li>
+                  <li>
+                    <a href="">Change Password</a>
+                  </li>
+                  <li>
+                    <a href="" onClick={logout} >Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-9"></div>
+        </div>
+      </div>
     </Layout>
     )
 }
