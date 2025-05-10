91ea0949e6580a54dbff1dd7bb32fa06be494199
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-10 08:58:57 +0530

    feat(FeaturedProducts Compoent) fetched featured products and listed in section

--- src/components/common/FeaturedProducts.jsx
+++ src/components/common/FeaturedProducts.jsx
@@ -1,70 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductImg1 from '../../assets/images/Mens/six.jpg';
 import ProductImg2 from '../../assets/images/Mens/seven.jpg';
 import ProductImg3 from '../../assets/images/Mens/six.jpg';
 import ProductImg4 from '../../assets/images/Mens/fivee.jpg';
+import { adminToken, apiUrl } from './http';
 const FeaturedProducts = () => {
+
+    const [products, setProducts] = useState([]);
+
+    const getLatestProducts = async () => {
+        let res = await fetch(`${apiUrl}/get-featured-products`, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${adminToken()}`,
+            }
+        }).then(res => res.json())
+            .then(result => {
+                if (result.status == 200) {
+                    console.log(result)
+                    setProducts(result.data);
+                } else {
+                    console.log("something went wrong")
+                }
+            })
+    }
+
+    useEffect(() => {
+        getLatestProducts()
+    }, [])
+
     return (
         <section className="section-2 py-5">
             <div className="container">
                 <h2>Featured Products</h2>
                 <div className="row mt-4">
-                    <div className="col-md-3 col-6">
-                        {/* Product 1 */}
-                        <div className="product card border-0">
-                            <div className="card-img">
-                                <img src={ProductImg1} alt="" className='w-100' />
-                            </div>
-                            <div className="card-body pt-3">
-                                <a href="">Red Check Shirt For Men</a>
-                                <div className='price'>
-                                    $50 <span className='text-decoration-line-through'>$80</span>
+
+                    {
+                        products && products.map(product => {
+                            return (
+
+                                <div className="col-md-3 col-6">
+                                    <div className="product card border-0">
+                                        <div className="card-img">
+                                            {
+                                                product.image_url !== "" ? <img src={product.image_url} alt="" />
+                                                    : <img src={'https://placehold.co/360x540/png'} alt="" />
+                                            }
+
+                                        </div>
+                                        <div className="card-body pt-3">
+                                            <a href="">{product.title}</a>
+                                            <div className='price'>
+                                                ${product.price} <span className='text-decoration-line-through'>${product.compare_price}</span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Product 2 */}
-                    <div className="col-md-3 col-6">
-                        <div className="product card border-0">
-                            <div className="card-img">
-                                <img src={ProductImg2} alt="" className='w-100' />
-                            </div>
-                            <div className="card-body pt-3">
-                                <a href="">Red Check Shirt For Men</a>
-                                <div className='price'>
-                                    $50 <span className='text-decoration-line-through'>$80</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Product 3 */}
-                    <div className="col-md-3 col-6">
-                        <div className="product card border-0">
-                            <div className="card-img">
-                                <img src={ProductImg3} alt="" className='w-100' />
-                            </div>
-                            <div className="card-body pt-3">
-                                <a href="">Red Check Shirt For Men</a>
-                                <div className='price'>
-                                    $50 <span className='text-decoration-line-through'>$80</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Product 4 */}
-                    <div className="col-md-3 col-6">
-                        <div className="product card border-0">
-                            <div className="card-img">
-                                <img src={ProductImg4} alt="" className='w-100' />
-                            </div>
-                            <div className="card-body pt-3">
-                                <a href="">Red Check Shirt For Men</a>
-                                <div className='price'>
-                                    $50 <span className='text-decoration-line-through'>$80</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                            )
+                        })
+                    }
+
                 </div>
             </div>
         </section>
