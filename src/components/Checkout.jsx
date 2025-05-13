e0e816da5eb3dfba3a6ac828f201a120cd0810d0
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-13 08:03:27 +0530

    feat(Checkout) applied form validations to form fields listed dynamic cart items

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Layout from './common/Layout'
 import { Link } from 'react-router-dom'
-import ProductImg from '../assets/images/Mens/three.jpg';
+import { CartContext } from './context/Cart';
+import { useForm } from 'react-hook-form';
+
 
 
 const Checkout = () => {
+
+    const { cardData, grandTotal, subTotal, shipping, updatedCartItem, deleteCartItem } = useContext(CartContext)
     const [paymenMethod, setPaymentMethod] = useState('cod');
 
-    const handlePaymentMethod=(e)=>{
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+
+    const handlePaymentMethod = (e) => {
         setPaymentMethod(e.target.value)
+    }
+
+
+    const onProcessOrder = (data) => {
+        console.log(data)
     }
     return (
         <Layout>
@@ -23,103 +40,176 @@ const Checkout = () => {
                         </nav>
                     </div>
                 </div>
-
-                <div className="row">
-                    <div className="col-md-7">
-                        <h3 className='border-bottom pb-3'><strong>Billing Details</strong></h3>
-                        <form action="">
+                <form action="" onSubmit={handleSubmit(onProcessOrder)} >
+                    <div className="row">
+                        <div className="col-md-7">
+                            <h3 className='border-bottom pb-3'><strong>Billing Details</strong></h3>
                             <div className="row pt-3">
                                 <div className="col-md-6">
                                     <div className="mb-3">
-                                        <input type="text" className='form-control' placeholder='name' />
+                                        <input type="text"
+                                            {
+                                            ...register("name", {
+                                                required: "the Name field is required."
+                                            })
+                                            }
+                                            className={`form-control ${errors.name && 'is-invalid'}`} placeholder='name' />
+                                        {
+                                            errors.name && <p className='invalid-feedback'>{errors.name?.message}</p>
+                                        }
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="mb-3">
-                                        <input type="text" className='form-control' placeholder='email' />
+                                        <input
+                                            {
+                                            ...register("email", {
+                                                required: "the Email field is required."
+                                            })
+                                            }
+                                            type="text" className={`form-control ${errors.email && 'is-invalid'}`} placeholder='email' />
+                                        {
+                                            errors.email && <p className='invalid-feedback'>{errors.email?.message}</p>
+                                        }
                                     </div>
                                 </div>
                                 <div className="mb-3">
-                                    <textarea className='form-control' placeholder='address' cols="30" rows="5"></textarea>
+                                    <textarea
+                                        {
+                                        ...register("address", {
+                                            required: "the Address field is required."
+                                        })
+                                        }
+                                        className={`form-control ${errors.address && 'is-invalid'}`} placeholder='address' cols="30" rows="5"></textarea>
+                                    {
+                                        errors.address && <p className='invalid-feedback'>{errors.address?.message}</p>
+                                    }
                                 </div>
                                 <div className="col-md-6">
                                     <div className="mb-3">
-                                        <input type="text" className='form-control' placeholder='city' />
+                                        <input
+                                            {
+                                            ...register("city", {
+                                                required: "the City field is required."
+                                            })
+                                            }
+                                            type="text" className={`form-control ${errors.city && 'is-invalid'}`} placeholder='city' />
+                                        {
+                                            errors.city && <p className='invalid-feedback'>{errors.city?.message}</p>
+                                        }
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="mb-3">
-                                        <input type="text" className='form-control' placeholder='state' />
+                                        <input
+                                            {
+                                            ...register("state", {
+                                                required: "the State field is required."
+                                            })
+                                            }
+                                            type="text"
+                                            className={`form-control ${errors.state && 'is-invalid'}`}
+                                            placeholder='state' />
+                                        {
+                                            errors.state && <p className='invalid-feedback'>{errors.state?.message}</p>
+                                        }
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="mb-3">
-                                        <input type="text" className='form-control' placeholder='zip' />
+                                        <input
+                                        {
+                                            ...register("zip", {
+                                                required: "the Zip field is required."
+                                            })
+                                            }
+                                        type="text" className={`form-control ${errors.zip && 'is-invalid'}`} placeholder='zip' />
+                                             {
+                                        errors.zip && <p className='invalid-feedback'>{errors.address?.message}</p>
+                                    }
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="mb-3">
-                                        <input type="text" className='form-control' placeholder='mobile' />
+                                        <input 
+                                        
+                                        {
+                                            ...register("mobile", {
+                                                required: "the Mobile field is required."
+                                            })
+                                            }
+                                        type="text" className={`form-control ${errors.mobile && 'is-invalid'}`} placeholder='mobile' />
+                                             {
+                                        errors.mobile && <p className='invalid-feedback'>{errors.mobile?.message}</p>
+                                    }
                                     </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div className="col-md-5">
-                        <h3 className='border-bottom pb-3'><strong>Items</strong></h3>
-                        <table className="table">
-                            <tbody>
-                                <tr>
-                                    <td width={100}>
-                                        <img src={ProductImg} width={80} alt="" />
-                                    </td>
-                                    <td width={600}>
-                                        <h4>Dummy Project Title</h4>
-                                        <div className="align-items-center d-flex pt-3">
-                                            <span>$10</span>
-                                            <div className="ps-3">
-                                                <button className="btn btn-size">S</button>
-                                            </div>
-                                            <div className="ps-5">x 1</div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="d-flex justify-content-between border-bottom pb-2">
-                                    <div><strong>Subtotal</strong></div>
-                                    <div>$20</div>
-                                </div>
-                                <div className="d-flex justify-content-between border-bottom py-2">
-                                    <div><strong>Shipping</strong></div>
-                                    <div>$5</div>
-                                </div>
-                                <div className="d-flex justify-content-between border-bottom py-2">
-                                    <div><strong>Grand Total</strong></div>
-                                    <div>$25</div>
-                                </div>
-                                <div className="d-flex justify-content-end  py-3">
-                                    <button className="btn btn-primary">Procceed to checkout</button>
                                 </div>
                             </div>
                         </div>
 
-                        <div>
-                            <input type="radio"  onClick={handlePaymentMethod} checked={paymenMethod === 'stripe'} value={'stripe'} name="" id="stripeRadio" />
-                            <label htmlFor="stripeRadio" className='form-label ps-2'>Stripe</label>
-                            <input onClick={handlePaymentMethod}  type="radio" checked={paymenMethod === 'cod'} value={'cod'} name="" id="codRadio" className='ms-3' />
-                            <label htmlFor="codRadio" className='form-label ps-2'>COD</label>
-                        </div>
+                        <div className="col-md-5">
+                            <h3 className='border-bottom pb-3'><strong>Items</strong></h3>
+                            <table className="table">
+                                <tbody>
+                                    {
+                                        cardData && cardData.map(cartItem => {
+                                            return (
+                                                <tr key={`cartitem-${cartItem.id}`} >
+                                                    <td width={100}>
+                                                        <img src={cartItem.image_url} width={80} alt="" />
+                                                    </td>
+                                                    <td width={600}>
+                                                        <h4>{cartItem.title}</h4>
+                                                        <div className="align-items-center d-flex pt-3">
+                                                            <span>${cartItem.price}</span>
+                                                            <div className="ps-3">
+                                                                {
+                                                                    cartItem.size && <button className="btn btn-size">{cartItem.size}</button>
+                                                                }
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
 
-                        <h3 className='border-bottom pt-4 pb-3'><strong>Payment Methods</strong></h3>
-                        <div className="d-flex py-3">
-                                    <button className="btn btn-primary">Pay Now</button>
+                                </tbody>
+                            </table>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="d-flex justify-content-between border-bottom pb-2">
+                                        <div><strong>Subtotal</strong></div>
+                                        <div>$20</div>
+                                    </div>
+                                    <div className="d-flex justify-content-between border-bottom py-2">
+                                        <div><strong>Shipping</strong></div>
+                                        <div>$5</div>
+                                    </div>
+                                    <div className="d-flex justify-content-between border-bottom py-2">
+                                        <div><strong>Grand Total</strong></div>
+                                        <div>$25</div>
+                                    </div>
+                                    <div className="d-flex justify-content-end  py-3">
+                                        <button className="btn btn-primary">Procceed to checkout</button>
+                                    </div>
                                 </div>
+                            </div>
+
+                            <div>
+                                <input type="radio" onClick={handlePaymentMethod} checked={paymenMethod === 'stripe'} value={'stripe'} name="" id="stripeRadio" />
+                                <label htmlFor="stripeRadio" className='form-label ps-2'>Stripe</label>
+                                <input onClick={handlePaymentMethod} type="radio" checked={paymenMethod === 'cod'} value={'cod'} name="" id="codRadio" className='ms-3' />
+                                <label htmlFor="codRadio" className='form-label ps-2'>COD</label>
+                            </div>
+
+                            <h3 className='border-bottom pt-4 pb-3'><strong>Payment Methods</strong></h3>
+                            <div className="d-flex py-3">
+                                <button className="btn btn-primary">Pay Now</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </Layout>
     )
