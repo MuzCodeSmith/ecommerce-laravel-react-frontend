949214ad4f3bf83f9687149a08e608378978eb29
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-15 08:13:07 +0530

    feat(Confirmation.jsx) fetched Order Details based on Id from route displayed fetched order details

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Layout from './common/Layout'
+import { Link, useParams } from 'react-router-dom';
+import { apiUrl, userToken } from './common/http';
+import { CartContext } from './context/Cart';
+
 
 const Confirmation = () => {
+  const { grandTotal, subTotal, shipping} = useContext(CartContext)
+  const [order, setOrder] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  const params = useParams();
+
+  const fetchOrder = async () => {
+    fetch(`${apiUrl}/get-order-details/${params.id}`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${userToken()}`
+      }
+    }).then(res => res.json())
+      .then(result => {
+        if (result.status === 200) {
+          setLoading(false)
+          setOrder(result.data)
+          setItems(result.data.items)
+          console.log(result)
+        }
+      })
+  }
+
+  useEffect(() => {
+    fetchOrder();
+  }, [])
+
+
   return (
+
     <Layout>
       <div className="container py-5">
+        {
+          loading === true &&
+          <div className="text-center py-5">
+            <div className="spinner-border" role='status'>
+              <span className="visaully-hidden"></span>
+            </div>
+          </div>
+        }
+        {
+          loading === false && order &&
+          <div>
             <div className="row">
               <h1 className="text-center fw-bold text-success">Thank You!</h1>
               <p className="text-muted text-center">Your Order has been successfully placed.</p>
@@ -15,18 +61,30 @@ const Confirmation = () => {
                 <hr />
                 <div className="row">
                   <div className="col-6">
-                    <p><strong>Order ID: </strong>#12212</p>
-                    <p><strong>Date: </strong>17 March 2025</p>
+                    <p><strong>Order ID: </strong>{order.id}</p>
+                    <p><strong>Date: </strong>{order.created_at}</p>
                     <p>
-                      <strong>Status:</strong>
-                      <span className="badge badge-warnning">Pending</span>
+                      <strong>Status:</strong>&nbsp;
+                      {
+                        order.status == 'pending' && <span className="badge text-bg-warning">Pending</span>
+                      }
+                      {
+                        order.status == 'shipped' && <span className="badge text-bg-warning">Shipped</span>
+                      }
+                      {
+                        order.status == 'delivered' && <span className="badge text-bg-success">Delivered</span>
+                      }
+                      {
+                        order.status == 'cancelled' && <span className="badge text-bg-danger">Cancelled</span>
+                      }
+
                     </p>
                     <p><strong>Payment Method: </strong> COD</p>
                   </div>
                   <div className="col-6">
-                    <p><strong>Customer: </strong>Muzaffar Shaikh</p>
-                    <p><strong>Address: </strong>Dummy Address</p>
-                    <p><strong>Contact: </strong>8208091113</p>
+                    <p><strong>Customer: </strong>{order.name}</p>
+                    <p><strong>Address: </strong>{order.address}, {order.city}, {order.state}, {order.zip}</p>
+                    <p><strong>Contact: </strong>{order.mobile}</p>
                   </div>
                 </div>
                 <div className="row">
@@ -41,37 +99,50 @@ const Confirmation = () => {
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td>Product 1</td>
-                          <td>100</td>
-                          <td>$10</td>
-                          <td>10</td>
-                        </tr>
-                      </tbody>
+                          {
+                            items && items.map(item => (
+                              <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.qty}</td>
+                                <td>${item.unit_price}</td>
+                                <td>${item.price}</td>
+                              </tr>
+                            ))
+                          }
+                        </tbody>
+
                       <tfoot>
                         <tr>
                           <td className='text-end fw-bold' colSpan={3}>Subtotal</td>
-                          <td>$10</td>
+                          <td>${order.subtotal}</td>
                         </tr>
                         <tr>
                           <td className='text-end fw-bold' colSpan={3}>Shipping</td>
-                          <td>$0</td>
+                          <td>${order.shipping}</td>
                         </tr>
                         <tr>
                           <td className='text-end fw-bold' colSpan={3}>Grand total</td>
-                          <td>$10</td>
+                          <td>${order.grand_total}</td>
                         </tr>
                       </tfoot>
                     </table>
                   </div>
                   <div className="text-center">
-                    <button className="btn btn-primary">Wiew Order Details</button>
-                    <button className="btn btn-outline-secondary ms-2">Continue Shopping</button>
+                    <button className="btn btn-primary">View Order Details</button>
+                    <Link to='/' className="btn btn-outline-secondary ms-2">Continue Shopping</Link>
                   </div>
                 </div>
               </div>
             </div>
-        </div>
+          </div>
+        }
+        {
+          loading === false && order.length === 0 &&
+          <div className="row">
+            <h1 className="text-center fw-bold text-muted">Order Not Found!</h1>
+          </div>
+        }
+      </div>
     </Layout>
   )
 }
