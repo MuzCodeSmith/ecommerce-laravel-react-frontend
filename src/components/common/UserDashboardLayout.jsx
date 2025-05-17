222c4731a3fc8a7d8dacd0ca5b00e9286675eac2
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-17 21:12:07 +0530

    refactor(UserDashboardLayout) : handle conditional card shadow

--- src/components/common/UserDashboardLayout.jsx
+++ src/components/common/UserDashboardLayout.jsx
@@ -3,7 +3,7 @@ import Layout from './Layout'
 import UserSidebar from './UserSidebar'
 import { Link } from 'react-router-dom'
 
-const UserDashboardLayout = ({pagetitle,btnLabel,btnLink="", children}) => {
+const UserDashboardLayout = ({pagetitle,btnLabel,btnLink="", children, innerShadow=true}) => {
   return (
     <Layout>
     <div className="container">
@@ -18,8 +18,8 @@ const UserDashboardLayout = ({pagetitle,btnLabel,btnLink="", children}) => {
           <UserSidebar/>
         </div>
         <div className="col-md-9">
-          <div className="card shadow mb-5">
-            <div className="card-body p-4">
+          <div  className={innerShadow ? "card shadow mb-5" :""}>
+            <div className={innerShadow?"card-body p-4":"card-body"}>
                 {children}
             </div>
           </div>
