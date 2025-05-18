599c1862160c6154c9815ca92b77eef262693bcf
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-18 11:21:38 +0530

    add shipping details incart context so it can gloabally accessible and updated logic accordingly

--- src/components/context/Cart.jsx
+++ src/components/context/Cart.jsx
@@ -1,58 +1,60 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { apiUrl } from "../common/http";
 
 export const CartContext = createContext();
 
-export const CartProvider = ({children}) =>{
-    const [cardData, setCartdata] = useState(JSON.parse(localStorage.getItem('cart')) ||[]);
+export const CartProvider = ({ children }) => {
+    const [cardData, setCartdata] = useState(JSON.parse(localStorage.getItem('cart')) || []);
+    const [shippingCost, setShippingCost] = useState(0);
 
-    const addToCart = (product, size=null) =>{
+    const addToCart = (product, size = null) => {
         let updatedCart = [...cardData];
 
         // if cart is empty
-        if(cardData.length == 0){
+        if (cardData.length == 0) {
             updatedCart.push({
-                id:`${product.id}-${Math.floor(Math.random()*1000000)}`,
-                product_id:product.id,
-                size:size,
-                title:product.title,
-                price:product.price,
-                qty:1,
-                image_url:product.image_url,
+                id: `${product.id}-${Math.floor(Math.random() * 1000000)}`,
+                product_id: product.id,
+                size: size,
+                title: product.title,
+                price: product.price,
+                qty: 1,
+                image_url: product.image_url,
             })
-        }else{
-            if(size !=null){
-                const isProducExist = updatedCart.find(item=>item.product_id == product.id && item.size == size)
+        } else {
+            if (size != null) {
+                const isProducExist = updatedCart.find(item => item.product_id == product.id && item.size == size)
 
-                if(isProducExist){
-                    updatedCart= updatedCart.map(item=>
-                        (item.product_id == product.id && item.size == size) ? {...item,qty:item.qty+1} : item
-                        )
-                }else{
+                if (isProducExist) {
+                    updatedCart = updatedCart.map(item =>
+                        (item.product_id == product.id && item.size == size) ? { ...item, qty: item.qty + 1 } : item
+                    )
+                } else {
                     updatedCart.push({
-                        id:`${product.id}-${Math.floor(Math.random()*1000000)}`,
-                        product_id:product.id,
-                        size:size,
-                        title:product.title,
-                        price:product.price,
-                        qty:1,
-                        image_url:product.image_url,
+                        id: `${product.id}-${Math.floor(Math.random() * 1000000)}`,
+                        product_id: product.id,
+                        size: size,
+                        title: product.title,
+                        price: product.price,
+                        qty: 1,
+                        image_url: product.image_url,
                     })
                 }
-            }else{
-                const isProducExist = updatedCart.find(item=>item.product_id == product.id)
+            } else {
+                const isProducExist = updatedCart.find(item => item.product_id == product.id)
 
-                if(isProducExist){
-                    updatedCart= updatedCart.map(item=>
-                        (item.product_id == product.id) ? {...item,qty:item.qty+1} : item)
-                }else{
+                if (isProducExist) {
+                    updatedCart = updatedCart.map(item =>
+                        (item.product_id == product.id) ? { ...item, qty: item.qty + 1 } : item)
+                } else {
                     updatedCart.push({
-                        id:`${product.id}-${Math.floor(Math.random()*1000000)}`,
-                        product_id:product.id,
-                        size:size,
-                        title:product.title,
-                        price:product.price,
-                        qty:1,
-                        image_url:product.image_url,
+                        id: `${product.id}-${Math.floor(Math.random() * 1000000)}`,
+                        product_id: product.id,
+                        size: size,
+                        title: product.title,
+                        price: product.price,
+                        qty: 1,
+                        image_url: product.image_url,
                     })
                 }
             }
@@ -60,46 +62,72 @@ export const CartProvider = ({children}) =>{
 
 
         setCartdata(updatedCart);
-        localStorage.setItem('cart',JSON.stringify(updatedCart));
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
     }
 
-    const shipping = () =>{
-        return 0;
+    const fetchShippingCharge=async()=>{
+        let res = await fetch(`${apiUrl}/get-shipping-front`, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then(response => response.json())
+            .then(result => {
+                console.log(result)
+                if (result.status == 200) {
+                    setShippingCost(result.data.shipping_charge);
+                } else {
+                    setShippingCost(0);
+                    console.log("something went wrong")
+                }
+            })
     }
-    const subTotal = () =>{
-        let subTotal = 0;
+
+    const shipping = () => {
+        let shippingAmmount = 0;
         cardData.map(item=>{
+            shippingAmmount += item.qty * shippingCost;
+        })
+        return shippingAmmount;
+    }
+    const subTotal = () => {
+        let subTotal = 0;
+        cardData.map(item => {
             subTotal += Number(item.qty) * Number(item.price);;
         })
         return Math.round(subTotal);
     }
-    const grandTotal = () =>{
-        return  subTotal() + shipping();
+    const grandTotal = () => {
+        return subTotal() + shipping();
     }
 
-    const updatedCartItem = (itemId,newQty)=>{
+    const updatedCartItem = (itemId, newQty) => {
         let updatedCart = [...cardData];
-        updatedCart = updatedCart.map(item=>(item.id==itemId)?{...item, qty:newQty}:item);
+        updatedCart = updatedCart.map(item => (item.id == itemId) ? { ...item, qty: newQty } : item);
         setCartdata(updatedCart);
-        localStorage.setItem('cart',JSON.stringify(updatedCart));
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
     }
 
-    const deleteCartItem = (itemId) =>{
-        const newCartData= cardData.filter(item=>item.id != itemId);
+    const deleteCartItem = (itemId) => {
+        const newCartData = cardData.filter(item => item.id != itemId);
         setCartdata(newCartData);
-        localStorage.setItem('cart',JSON.stringify(newCartData));
+        localStorage.setItem('cart', JSON.stringify(newCartData));
     }
 
-    const getQty = () =>{
+    const getQty = () => {
         let qty = 0;
-        cardData.map(item=>{
+        cardData.map(item => {
             qty += parseInt(item.qty)
         });
         return qty;
     }
 
-    return(
-        <CartContext.Provider value={{addToCart,cardData,grandTotal,subTotal,shipping,deleteCartItem,getQty,updatedCartItem}} >
+    useEffect(()=>{
+        fetchShippingCharge();
+    },[])
+    return (
+        <CartContext.Provider value={{ addToCart, cardData, grandTotal, subTotal, shipping, deleteCartItem, getQty, updatedCartItem }} >
             {children}
         </CartContext.Provider>
     )
